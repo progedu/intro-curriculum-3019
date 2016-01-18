abd4ea5f2a7b731ed8377cc2ddc4ebeedc7f215a
Davideryu_Orihara <davideryu_orihara@dwango.co.jp>
2016-01-18 03:18:20 +0000

    アクセスするとニコニコ動画のトップページにリダイレクトする処理を追加wq

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 let http = require('http');
 let server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://www.nicovideo.jp/'
+  });
   res.end();
 });
 let port = 8000;
