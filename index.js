de32ac5c80070093e12e4f1a8dc3c1aee1cbc8fc
Yukimasa Shimada <github@eleos.jp>
2016-11-01 12:13:29 +0000

    N予備校へリダイレクト

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 const http = require('http');
+const redirectURL = 'http://www.nnn.ed.nico/';
 const server = http.createServer((req, res) => {
+  res.writeHead(302,{
+    'Location': redirectURL
+    });
+    console.info(redirectURL + ' へ リダイレクトしました。');
   res.end();
 });
 const port = 8000;
