e028936924323289f2cd283c06bcfa5555311e7f
ninja6331 <ninja6331@gmail.com>
2016-11-01 06:31:23 +0000

    リダイレクトの実装

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+	res.writeHead(302, {
+		'Location': 'http://ch.nicovideo.jp/channel/ch2611381'
+	});
   res.end();
 });
 const port = 8000;
