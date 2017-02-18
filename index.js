04c393c4417a67da9798f5e92177c09137d5fd90
tksfj17 <tksfj17@gmail.com>
2017-02-18 10:19:16 +0000

    N予備校にリダイレクトするように変更

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(302, {
-    'Location': 'http://www.nicovideo.jp/'
+    'Location': 'http://www.nnn.ed.nico/'
   });
   res.end();
 });
