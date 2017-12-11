eff449e6436450d6b5928fc506284460d14c747d
wada <papercup.wada@gmail.com>
2017-12-11 21:06:12 +0000

    niconicoへのリダイレクト

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end();
+  res.writeHead(302, {
+    'Location' : 'http://www.nicovideo.jp/'
+  });
+res.end();
 });
 const port = 8000;
 server.listen(port, () => {
