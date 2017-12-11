0d370b278f4667dc3ed3bc2a1d4ca22f0aaeb034
Makoto Kitakata <h7QEfr@gmail.com>
2017-12-11 13:53:24 +0000

    リダイレクト

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'https://www.aeonshop.com/shop/genre/genre.aspx?shop=01050000059020&genre=2145&ismodesmartphone=off&bannerid=as25_DMwEO7BF&exmid=CPC'
+  });
   res.end();
 });
 const port = 8000;
