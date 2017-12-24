d2c3ac3bc8d8fb9b6759d000aa3db03ff689b9a5
apprec100 <34231250+apprec100@users.noreply.github.com>
2017-12-24 11:45:55 +0900

    リダイレクト先を変更

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://www.nicovideo.jp/'
+  });
   res.end();
+  console.log('リダイレクトされました。');  
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
