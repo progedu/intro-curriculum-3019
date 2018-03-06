2ba394b9344b9c263339597430f56532304fa3ce
dande555lion <dande555lion@yahoo.co.jp>
2018-03-06 12:45:59 +0000

    リダイレクトを実装：宇多田ヒカルver

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://www.utadahikaru.jp/'
+  });
   res.end();
 });
 const port = 8000;
