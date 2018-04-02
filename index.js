451e513535e33eb19ac8bdf764ae4c91af660b02
moyasin2018 <moyasin2006@gmail.com>
2018-04-02 03:56:04 +0000

    niconico動画へのリダイレクト

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHeader(302, {
+    'Location':'http://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
