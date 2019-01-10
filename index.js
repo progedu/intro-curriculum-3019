fa5ead59cb2d0b6804ddefbcc27ac6f88bff22ef
Yusuke Kukita <kukita@rightcode.co.jp>
2019-01-10 03:17:41 +0000

    リダイレクトを設定

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(301, {
+    'Location': 'http://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
