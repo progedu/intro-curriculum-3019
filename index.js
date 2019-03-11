361d37a6a38ed6fb0a7e42b6d91a0e15eee507e2
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2019-03-11 06:39:49 +0000

    ニコニコインフォページへのリダイレクションを追加

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://blog.nicovideo.jp/niconews/67092.html'
+  });
   res.end();
 });
 const port = 8000;
