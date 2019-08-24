27f6db512981c83a8ae7474b9092d25658b5f2d0
Rooooo18 <h.rooooo18@gmail.com>
2019-08-24 12:39:42 +0900

    リダイレクト機能の実装

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'https://qiita.com/kotakanbe@github/items/32cf4eb3de1741af26fb'
+  });
   res.end();
 });
 const port = 8000;
