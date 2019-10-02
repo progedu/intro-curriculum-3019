93d8f0c2c5378cca93da400ae4c56af291f94879
gtpmm <bluephysics33@gmail.com>
2019-10-02 19:50:08 +0900

    ニコニコにリダイレクト

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  // 間違い回答
+  // req.headers(302,{
+  //   'Location': 'https://www.nicovideo.jp/'
+  // });
+
+  res.writeHead(302,'test reasonPhrase',{
+    'Location':'https://www.nicovideo.jp/'
+  });
+  console.info('試しにリーズンフレーズ指定：'+ res.statusMessage);
   res.end();
 });
 const port = 8000;
