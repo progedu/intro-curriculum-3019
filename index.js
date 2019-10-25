fb568b67a53cec24248fc94e1d18f155298d5a51
Asazuke <quusan3@gmail.com>
2019-10-25 20:03:44 +0900

    ニコニコ動画へリダイレクト＆ファビコン作成ページ

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'https://www.nicovideo.jp/'
+  });
+  
+  //簡単に画像ファイルをファビコンを作成できるページ
+  // res.writeHead(302,{
+  //   'Location':'https://ao-system.net/favicon/'
+  // });
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
