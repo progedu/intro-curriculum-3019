432e2d4c4ff09c9c1225b25921c7a7ddc8f2c63b
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2020-01-10 02:03:52 +0900

    更新を押すとリダイレクトする処理の実装

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(302, {
-    'Location': 'http://blog.nicovideo.jp/niconews/67092.html'
-  });
-  res.end();
+
+  //resでクッキーに入れて
+  //reqに値が入る
+  const now = 'unredirect';
+//console.info(req.url);
+  if(req.url === '/')
+  {
+    if(req.headers.cookie !== `now=${now}`)
+      {
+        res.setHeader('Set-Cookie',`now=${now};`);
+        //console.info(`リダイレクト後:${req.headers.cookie}`);
+        res.writeHead(302, {
+          'Location': 'https://www.nicovideo.jp/'
+        });
+      } else {
+        res.setHeader('Set-Cookie',`now=redirect;`);
+        //console.info(`リダイレクト前:${req.headers.cookie}`);
+      }
+      //console.info(`----------`);
+      res.end('Please Enter F5');
+  }
 });
 const port = 8000;
 server.listen(port, () => {
