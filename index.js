3b6cb37a779d7db1cdda4c97a3debfb6526dbaa6
ada1897 <lifeispartymyguitar@gmail.com>
2020-01-12 11:00:33 +0900

    ニコニコ動画にリダイレクトさせる

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'location': 'https://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
