97c754179a2b51fb553fc394ad31197bf4f3d8ec
dora926 <morikawa926ryu@gmail.com>
2020-03-06 00:22:06 +0900

    ニコ動TOP にリダイレクトされるように修正

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(301, {
+    'Location': 'https://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
