c2dc9d558f59e548192b355dcce0de91d65701d0
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-04-08 22:07:33 +0900

    ニコ動にリダイレクトし、status codeとredirect先をログ上に表示

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'https://www.nicovideo.jp/'
+  });
+  res.statusCode = 302;
+  console.info(`ステータスコードは ${res.statusCode} です。`);
+  console.info('ニコニコ動画にリダイレクトしました。');
   res.end();
 });
 const port = 8000;
