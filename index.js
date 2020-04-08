d1d0e273ca205b7a96c709e63b7785394d2d9833
koshin2020 <koushin20085@gmail.com>
2020-04-08 14:43:07 +0900

    リダイレクト機能の実装

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.write(302, {
+    'Location': 'https://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
