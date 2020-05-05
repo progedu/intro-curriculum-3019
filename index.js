4660ca7be64ecb28ab2a17247820b7c88910ae69
Haggy Kusanagi <artkit44t@gmail.com>
2020-05-05 12:42:36 +0900

    リダイレクト実装

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.end();
 });
+
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
