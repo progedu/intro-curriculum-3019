e74609bfd2d709e0d713a1873790122258154102
ymtk <ymuc10tkm18@outlook.jp>
2020-06-01 21:47:28 +0900

    リダイレクトを設定

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    Location: 'https://www.nnn.ed.nico/'
+  });
   res.end();
 });
 const port = 8000;
