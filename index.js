e1b18790f77fad01e76b3d00588730b34a8b6c6d
suissan <programming0607@gmail.com>
2020-10-29 14:39:10 +0900

    別サイトへのリダイレクト処理を実装

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  const now = new Date();
+  const saenaiURL = 'https://saenai-movie.com/';
+  res.writeHead(302, {
+    Location: saenaiURL
+  });
+  console.info(`[ ${now} ] ${saenaiURL} へのリダイレクト完了`);
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+  console.info(`Listening on ${port}`);
 });
-
