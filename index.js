b02e468518d6ec1562604920a6a7af398e8ba093
vagrant <vagrant@vagrant.vm>
2020-11-06 21:20:18 +0900

    pugでリダイレクト

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 const http = require('http');
+const pug = require('pug');
 const server = http.createServer((req, res) => {
+  // res.writeHead(302, {
+  //   Location: 'https://www.nicovideo.jp'
+  // });
+  res.writeHead(200, {
+    'Context-Type': 'text/html; charset=utf-8'
+  });
+  res.write(
+    pug.renderFile('./redirect.pug', { seconds: 2 })
+  );
   res.end();
 });
 const port = 8000;
