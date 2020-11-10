d5f2f6ea830065aa9efcdcdcc81900e634127f34
hsekido <67943923+hsekido@users.noreply.github.com>
2020-11-10 20:04:48 +0900

    リダイレクトされる HTTP サーバーを実装
    久しぶりにプルリク間に合いました。

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writehead(302, {
+    Location: 'https://www.nicovideo.jp/'
+  });  
   res.end();
 });
 const port = 8000;
