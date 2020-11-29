506f82cbac1b41020625e5615c7b432ccacda984
Yuu Ayakawa <ayakawayuu@ayakawayuu.dev>
2020-11-29 19:28:28 +0900

    課題完了

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    Location: 'https://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
