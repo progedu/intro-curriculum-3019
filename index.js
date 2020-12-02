82215360f872096f82e21529d02c2511c7d18943
gomi <assassincreed0461-gta005@outlook.jp>
2020-12-02 18:03:42 +0900

    細かな修正。

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(302 , {
     Location: 'https://www.nicovideo.jp/'
-  })
+  });
   res.end();
 });
 const port = 8000;
